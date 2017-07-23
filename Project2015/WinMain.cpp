9960a2c9f0746171aa0d972dc469f7f3362c81de
yamada <a>
2017-07-23 21:58:23 +0900

    メニューバー実装、描画できるようにした。

--- Project2015/WinMain.cpp
+++ Project2015/WinMain.cpp
@@ -1,59 +1,139 @@
 #include "DxLib.h"
+#include "resource.h"
 
 const LPCWSTR CLASS_NAME = L"ImageEditor";
-const LPCWSTR TITLE_NAME = L"ImageEditor";
+const char* TITLE_NAME = "ImageEditor";
 const int INIT_X = 10;
 const int INIT_Y = 10;
 const int INIT_WIDTH = 1280;
 const int INIT_HEIGHT = 720;
 
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
+int createWindow( HWND* hWnd, HINSTANCE hInstance );
+int processMessage( HWND hWnd, int nCmdShow );
+
+
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
+	//MAKEINTRESOURCE( IDR_MENU1 );
+	// ウィンドウを生成する
+	HWND hWnd = NULL;
+	if ( createWindow( &hWnd, hInstance ) == FALSE ) {
+		MessageBox( NULL, "ウィンドウの作成に失敗しました", "エラー", MB_OK );
+		return GetLastError( );
+	}
+	DxLib::SetUserWindow( hWnd );
+	ShowWindow( hWnd, nCmdShow );
+	if ( DxLib_Init( ) == -1 ) {
+		return -1;
+	}
+	SetDrawScreen( DX_SCREEN_BACK );
+	int x = 0;
+	int y = 0;
+	while ( processMessage( hWnd, nCmdShow ) == TRUE ) {
+		if ( CheckHitKey( KEY_INPUT_A ) != 0 ) {
+			int check = 0;
+		}
+		ClearDrawScreen( );
+		x++;
+		y++;
+		DrawBox( 0, 0, x, y, GetColor( 100, 0, 0 ), TRUE );
+		ScreenFlip( );
+	}
+	return 0;
+}
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam ) {
+	/*
+	HDC hdc;
+    PAINTSTRUCT ps;
+    static COLORREF color = RGB( 0, 0, 0 );
+	static int x = 0;
+	static int y = 0;
+    //渡された message から、イベントの種類を解析する
+    switch( msg ){
+    case WM_CREATE:
+        color = RGB( 0, 0, 0 );            //最初は黒色
+        x = 50;
+        y = 50;
+        break;
+    case WM_KEYDOWN:
+        switch(wParam){
+        case 'R':
+            color = RGB( 255, 0, 128 );    //'R' red 赤色
+            break;
+        case 'B':
+            color = RGB( 0, 128, 255 );    //'B' blue 青色
+            break;
+        case VK_RIGHT: if(x<100) x++; break;
+        case VK_LEFT:  if(x>  0) x--; break;
+        case VK_DOWN:  if(y<100) y++; break;
+        case VK_UP:    if(y>  0) y--; break;
+        }
+        InvalidateRect(hWnd, NULL, TRUE);
+        UpdateWindow(hWnd);
+        break;
+    //----ペイント----
+    case WM_PAINT:
+        hdc= BeginPaint (hWnd, &ps);
+        SetTextColor(hdc, color);
+		//SetPixel( hdc, x, y, color );
+        TextOut( hdc, x, y, "使用例 sample", 13 );
+        EndPaint(hWnd, &ps);
+        break;
+    //----終了処理----
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0L;
+    }
+	*/
+    //デフォルトの処理
+    return  DefWindowProc(hWnd,msg,wParam,lParam);
+}
+
+int createWindow( HWND* hWnd, HINSTANCE hInstance ) {
 	WNDCLASSEXW wc = { };
 	wc.cbSize			= sizeof( WNDCLASSEXW );
 	wc.style			= CS_CLASSDC;
-	wc.lpfnWndProc		= DefWindowProc;
+	wc.lpfnWndProc		= WndProc;
 	wc.cbClsExtra		= 0L;
 	wc.cbWndExtra		= 0L;
 	wc.hInstance		= hInstance;
 	wc.hIcon			= NULL;
 	wc.hCursor			= LoadCursor( NULL, IDC_ARROW );
 	wc.hbrBackground	= (HBRUSH)GetStockObject( WHITE_BRUSH );
-	wc.lpszMenuName		= NULL;
+	wc.lpszMenuName		= MAKEINTRESOURCEW( IDR_MENU1 );
 	wc.lpszClassName	= CLASS_NAME;
 	wc.hIconSm			= NULL;
 
-	//ウィンドウクラスの登録　エラー時は0が返ってくる
 	if ( RegisterClassExW( &wc ) == 0 ) {
-		MessageBox( NULL, "ウィンドウの作成に失敗しました", "エラー", MB_OK );
-		return GetLastError( );
+		return FALSE;
 	}
-
-
-	// ウィンドウを生成する
-	HWND window_handle = CreateWindowExW(
+	*hWnd = CreateWindowExW(
 		WS_EX_ACCEPTFILES,
-		CLASS_NAME, TITLE_NAME,
+		CLASS_NAME, LPCWSTR( ),
 		WS_OVERLAPPEDWINDOW,
-		INIT_X, INIT_Y,
-		INIT_WIDTH, INIT_HEIGHT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
 		NULL, NULL,
 		hInstance, NULL
 	);
-	if ( window_handle == NULL ) {
-		MessageBox( NULL, "ウィンドウの作成に失敗しました", "エラー", MB_OK );
-		return GetLastError( );
+	if ( !*hWnd ) {
+		return FALSE;
 	}
-	ShowWindow( window_handle, nCmdShow );
-	SetUserWindow( window_handle );
-	SetHookWinProc( DefWindowProc );
-	ChangeWindowMode( TRUE );
+	return TRUE;
+}
 
-	if ( DxLib_Init( ) == -1 ) {
-		return -1;
+
+int processMessage( HWND hWnd, int nCmdShow ) {
+	if ( CheckHitKey( KEY_INPUT_ESCAPE ) != 0 ) {
+		return FALSE;
 	}
-
-	while ( CheckHitKey( KEY_INPUT_ESCAPE ) == 0 ) {
-
+	if ( ProcessMessage( ) != 0 ) {
+		return FALSE;
 	}
-	return 0;
+	MSG Msg = MSG( );
+	if ( !GetMessage( &Msg, NULL, 0, 0 ) ) {
+		return FALSE;
+	}
+	return TRUE;
 }
---
+++ Project2015/resource.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define IDR_MENU1		4001
+#define IDM_FILEOPEN	4002
+#define IDM_MAKEFOLDER	4003
+#define IDM_EXIT		4004
+#define IDM_ABOUT		4005
